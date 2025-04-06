02e000556c2232281f9f3ce13aafbabb2f6d837f
Cesar Cervantes Lopez <Ces2131087@maricopa.edu>
2025-04-05 22:12:15 -0700

    commited files

---
+++ S25_Cervantes.md
@@ -0,0 +1,5 @@
+# song quote
+
+> Don't Sleep through dreams that can't come true, No more tears shall drop from your checks anymore. You wont need to strive for greatness, belive in me that you dont need to suffer from anything. 
+
+\- Throw Away your Mask by lyn
---
+++ S25_Cervantes.py
@@ -0,0 +1,3 @@
+print('"Don\'t Sleep through dreams that can\'t come true, No more tears shall drop from your checks anymore. You wont need to strive for greatness, belive in me that you dont need to suffer from anything."')
+print()
+print('"Throw Away Your Mask, by Lyn"')
